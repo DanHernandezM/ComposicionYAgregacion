c416e0cef4aa32dbef985b698b90c1657a7371de
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-11-21 23:24:39 -0600

    Pequeño arreglo en Agregacion, tarea terminada

--- src/main/java/com/mycompany/Ejemplos/Agregacion/Refrigerador.java
+++ src/main/java/com/mycompany/Ejemplos/Agregacion/Refrigerador.java
@@ -9,8 +9,15 @@ package com.mycompany.Ejemplos.Agregacion;
  * @author nenod
  */
 public class Refrigerador {
+public Refrigerador(){
+ System.out.println("*Refrigerador salvaje aparecio*");   
+}
+public void revisar(){
+    System.out.println("Quiero revisar el estado de mis alimentos!");
+}
 
     public static void main(String[] args) {
+        Refrigerador lg = new Refrigerador();
                 Mango mango = new Mango();
 		Berenjena berenjena = new Berenjena();
 		Cebolla cebolla = new Cebolla();
@@ -22,7 +29,8 @@ public class Refrigerador {
 		Pera pera = new Pera();
 		Sandia sandia = new Sandia();
 		
-		System.out.println("Quiero revisar el estado de mis alimentos");
+                lg.revisar();
+		
 		mango.estado();
                 berenjena.estado();
                 cebolla.estado();
